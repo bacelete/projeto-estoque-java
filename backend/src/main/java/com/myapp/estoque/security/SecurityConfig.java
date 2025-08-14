653ecabc579b6333c616d82068e783f5eb3e820e
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-08-14 13:46:42 -0300

    fix: i took of the local store logic

--- backend/src/main/java/com/myapp/estoque/security/SecurityConfig.java
+++ backend/src/main/java/com/myapp/estoque/security/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/auth/register").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .requestMatchers(HttpMethod.POST, "/produto").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
--- frontend/estoque/src/app/produto/all/page.js
+++ frontend/estoque/src/app/produto/all/page.js
@@ -3,31 +3,17 @@ import { useEffect, useState } from "react";
 import { useRouter } from 'next/navigation'
 
 export default function AllProducts() {
-    const [authenticated, setAuthentication] = useState(false);
     const [listaProdutos, setListaProdutos] = useState([]);
-    const router = useRouter();
-
 
     useEffect(() => {
-        const token = localStorage.getItem('token');
-
-        if (token != null) {
-            setAuthentication(true);
-        }
-        else {
-            setTimeout(() => {
-                router.push('/');
-            }, 2000)
-        }
-
         const fetchProdutos = async () => {
             try {
                 const response = await fetch('http://localhost:8080/produto/all', {
                     method: 'GET',
                     headers: {
-                        'Authorization': `Bearer ${token}`,
                         'Content-Type': 'application/json'
-                    }
+                    },
+                    credentials: 'include'
                 })
                 const lista = await response.json();
                 console.log(lista);
