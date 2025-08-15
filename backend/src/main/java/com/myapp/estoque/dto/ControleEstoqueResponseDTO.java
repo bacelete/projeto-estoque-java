f84c62875843bb356d6c32597f7cbfc14c5a9870
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-08-15 00:19:36 -0300

    feat: controle estoque response dto

--- backend/src/main/java/com/myapp/estoque/dto/ControleEstoqueResponseDTO.java
+++ backend/src/main/java/com/myapp/estoque/dto/ControleEstoqueResponseDTO.java
@@ -1,4 +1,14 @@
 package com.myapp.estoque.dto;
 
+import com.myapp.estoque.enums.TipoMovimentacao;
+import com.myapp.estoque.model.Produto;
+
+import java.time.LocalDateTime;
+
 public class ControleEstoqueResponseDTO {
+    private TipoMovimentacao tipoMovimentacao;
+    private double quantidade;
+    private LocalDateTime data_hora;
+    private String observacao;
+    private Produto produto;
 }
